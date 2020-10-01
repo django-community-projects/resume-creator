75d7cf12f1b9e3b886c7898cdabfbb0c10bbf588
sebastianmarines <18373185+sebastianmarines@users.noreply.github.com>
2020-10-01 17:03:30 -0500

    Closes #9
    * Install coverage to run tests.
    
    * Structure tests in directories
    
    * Create .coveragerc
    
    * Fix db path
    
    * Create the first test for the index page
    
    * Use reverse to get index url
    
    * Add comment to trigger action
    
    * Remove conflicting dependency
    
    * Add failing test to check action
    
    Co-authored-by: Sebastian Marines <sebastian0marines@gmail.com>

--- resume/tests/test_views.py
+++ resume/tests/test_views.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class ViewsTest(TestCase):
     def test_index_page(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('resume:index'))
+        self.assertEqual(response.status_code, 404)
